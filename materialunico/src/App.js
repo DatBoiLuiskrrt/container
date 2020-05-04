5dddcc48e0437a3210d7403159af1cf73c6d02f6
Luis Hernandez <datboiluiskrrt@gmail.com>
2020-05-04 14:34:57 -0500

    Added two inputs, learning how to use MaterialUI

--- materialunico/src/App.js
+++ materialunico/src/App.js
@@ -1,13 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import PhotoCamera from '@material-ui/icons/PhotoCamera';
+
+
+
 
 function App() {
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+      },
+    },
+    input: {
+      display: 'none',
+    },
+  }));
+
+  
+
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <h1>Hola</h1>
-    </div>
+    <div>
+    <Button 
+    variant="outlined"
+     color="primary"
+     size="large"
+     >
+      Hello World
+    </Button>
+   
+    
+      
+      <input
+        accept=""
+        className={classes.input}
+        id="contained-button-file"
+        multiple
+        type=""
+      />
+      <label htmlFor="contained-button-file">
+        <Button
+         variant="contained"
+         size="small"
+        color="secondary"
+           component="span"
+           
+           
+           >
+          Upload
+        </Button>
+      </label>
+      <input accept="image/*" className={classes.input} id="icon-button-file" type="" />
+      <label htmlFor="icon-button-file">
+        <IconButton color="primary" aria-label="upload picture" component="span"
+        rows={2}>
+          <PhotoCamera />
+        </IconButton>
+      </label>
+      </div>
+   
   );
-}
+};
+
 
 export default App;
