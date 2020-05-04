9591bc3503be6fc409623a9df4ba72ad822d2bf9
Luis Hernandez <datboiluiskrrt@gmail.com>
2020-05-04 15:07:51 -0500

    Added a Menu component and passed data to it from the app.js using props

--- materialunico/src/App.js
+++ materialunico/src/App.js
@@ -1,67 +1,15 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import PhotoCamera from '@material-ui/icons/PhotoCamera';
-
-
-
+import Menus from '../src/components/Menu';
 
 function App() {
 
-  const useStyles = makeStyles((theme) => ({
-    root: {
-      '& > *': {
-        margin: theme.spacing(1),
-      },
-    },
-    input: {
-      display: 'none',
-    },
-  }));
-
-  
-
-  const classes = useStyles();
-
   return (
     <div>
-    <Button 
-    variant="outlined"
-     color="primary"
-     size="large"
-     >
-      Hello World
-    </Button>
-   
+      <Menus
+       name="Luisito"
+        lastName="Comunica"
+        />
     
-      
-      <input
-        accept=""
-        className={classes.input}
-        id="contained-button-file"
-        multiple
-        type=""
-      />
-      <label htmlFor="contained-button-file">
-        <Button
-         variant="contained"
-         size="small"
-        color="secondary"
-           component="span"
-           
-           
-           >
-          Upload
-        </Button>
-      </label>
-      <input accept="image/*" className={classes.input} id="icon-button-file" type="" />
-      <label htmlFor="icon-button-file">
-        <IconButton color="primary" aria-label="upload picture" component="span"
-        rows={2}>
-          <PhotoCamera />
-        </IconButton>
-      </label>
       </div>
    
   );
---
+++ materialunico/src/components/Menu.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Menu from '@material-ui/core/Menu';
+
+
+
+
+const Menus = (props) => {
+    return (
+        <div>
+    <h1>Hello my name is {props.name} {props.lastName}</h1>
+
+        </div>
+    )
+};
+
+export default Menus;
